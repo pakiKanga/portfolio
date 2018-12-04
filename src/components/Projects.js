8b85e2b145e01b55812873aee245e793f803a409
PakiKanga <alinaeem2006@gmail.com>
2018-12-05 01:17:38 +1100

    CSS fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,14 +12,50 @@ class Projects extends Component {
             <div className="project-top-bar">
               <h3 className="project-heading">HeyFam</h3>
             </div>
-            <div className="project-summary">
-              <img align="left" src={heyFam} className="project-image">
 
-              </img>
-              <p className="project-info">
-                Find the most optimal location to meet up with friends
-                through a midpoint algorithm.
-              </p>
+            <div className="project-summary">
+              <div className="image-container">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              </div>
+
+              <div className="project-info-container">
+                <p className="project-info">
+                  Find the most optimal location to meet up with friends
+                  through a midpoint algorithm. I should really sleep but
+                  I am really bored. I need to add a craptonne of text to see
+                  what happens.
+                </p>
+              </div>
+
+
+
+              <div className="project-bottom-bar">
+                <span className="text-muted">Flask | Materialise</span>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="project">
+            <div className="project-top-bar">
+              <h3 className="project-heading">Meetup Point</h3>
+            </div>
+
+            <div className="project-summary">
+              <div className="image-container">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              </div>
+
+              <div className="project-info-container">
+                <p className="project-info">
+                  Find the most optimal location to meet up with friends
+                  through a midpoint algorithm. I should really sleep but
+                  I am really bored. I need to add a craptonne of text to see
+                  what happens.
+                </p>
+              </div>
+
+
 
               <div className="project-bottom-bar">
                 <span className="text-muted">Flask | Materialise</span>
