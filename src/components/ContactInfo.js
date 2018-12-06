4b0dca380913b7e10ed310d50e4d7c5461e18042
PakiKanga <alinaeem2006@gmail.com>
2018-12-06 19:01:43 +1100

    hello

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import './../css/contact.css'
 
 class ContactInfo extends Component {
   render() {
     return (
-      <div className="contact-container">
-        <span class="name-title">Shaikhspeare</span>
-      </div>
+      <ul className="contact-container">
+        <li>
+          <a href="https://www.linkedin.com/in/shaikhspeare/">
+            <i class="fab fa-linkedin contact-icon"></i>
+          </a>
+            </li>
+        <li>
+          <a href="mailto:ali.shaikh@sydney.edu.au">
+            <i class="fas fa-envelope contact-icon"></i>
+            </a>
+        </li>
+        <li>
+          <a href="https://github.com/pakiKanga/portfolio">
+          <i class="fab fa-github contact-icon"></i>
+          </a>
+        </li>
+
+      </ul>
     );
   }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import heyFam from './../images/HeyFam.PNG';
+import mPoint from './../images/MeetupPoint.PNG';
+import './../css/project.css'
+
 class Projects extends Component {
   constructor(props) {
     super(props);
     this.state = {
       projects: [
-        {id: 1, projectName: "HeyFam", summary: "Find the most convenient place to meet up", tech: "Materialise | Flask | MySQL", image: heyFam},
-        {id: 2, projectName: "Meetup Point", summary: "Find the most convenient place to meet up", tech: "React | Node | MySQL"},
+        {id: 1, projectName: "HeyFam", summary: "Sed eget dapibus lectus, eu sodales mi. In varius dapibus nibh, id faucibus diam dictum quis. Vestibulum eu mollis mauris. Nunc convallis elementum urna vitae tempor. In in mattis diam, vitae porttitor metus. Sed sit amet tincidunt turpis, vel scelerisque erat. Nunc turpis metus, sodales ac posuere iaculis, dignissim et diam. Curabitur malesuada et arcu vitae luctus. Aliquam erat volutpat. Quisque vel euismod augue. Ut non mauris nec lectus facilisis eleifend. Etiam ornare mi a ante convallis iaculis. Fusce dictum blandit venenatis. Duis vestibulum est feugiat risus fermentum, in tempor nibh semper. Suspendisse potenti.        ", tech: "Materialise | Flask | MySQL", image: heyFam},
+        {id: 2, projectName: "Meetup Point", summary: "Find the most convenient place to meet up", tech: "React | Node | MySQL", image: mPoint},
         {id: 3, projectName: "SpaceCraft VR", summary: "Find the most convenient place to meet up", tech: "Unreal Engine"},
         {id: 4, projectName: "Application Verification System", summary: "Applicants can fill and submit forms which are administrated by different tiers of administration, for the Pakistani Consulate.", tech: "Materialise | Flask | MySQL"}
 
@@ -17,14 +20,13 @@ class Projects extends Component {
   render() {
     const projectItems = this.state.projects.map((project) =>
       <div className="project">
+        <div className="image-container">
+        </div>
       <div className="project-top-bar">
         <h3 className="project-heading">{project.projectName}</h3>
       </div>
 
       <div className="project-summary">
-        <div className="image-container">
-          <img className="project-image" src={project.image}></img>
-        </div>
 
         <div className="project-info-container">
           <p className="project-info">
