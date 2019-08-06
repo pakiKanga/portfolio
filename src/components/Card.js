92405623132977d4fc8d60b686b41a50b10cbcc7
Ali Shaikh <ali.shaikh@sydney.edu.au>
2019-08-06 23:36:50 +1000

    Mobile responsive

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,42 +9,54 @@ class Card extends Component {
     this.cardRef = React.createRef();
     this.mainRef = React.createRef();
     this.cardBack = React.createRef();
-    this.handleClick = this.handleClick.bind(this);
-    this.flipCard = this.flipCard.bind(this);
+    this.flipBackward = this.flipBackward.bind(this);
+    this.flipForward = this.flipForward.bind(this);
     this.state = {
       flipped: false
     };
   }
 
-  handleClick() {
+  flipBackward() {
     const container = this.cardRef.current;
     const mainRef = this.mainRef.current;
     const cardBackRef = this.cardBack.current;
-
-
-      container.classList.toggle("flipped");
-      container.classList.toggle("unflipped");
-      container.classList.toggle("full-screen");
-      mainRef.classList.toggle("filled");
-      cardBackRef.classList.toggle("table-toggle");
-      this.setState({ flipped: true });
-    
-    console.log(this.cardRef.current.getBoundingClientRect());
+    container.classList.toggle("flipped");
+    container.classList.toggle("unflipped");
+    container.classList.toggle("full-screen");
+    mainRef.classList.toggle("filled");
+    cardBackRef.classList.toggle("table-toggle");
+    this.setState({ flipped: false });
   }
 
-  flipCard() {
-      this.setState({flipped: false});
-      console.log("flipped set to false");
+  flipForward() {
+    const container = this.cardRef.current;
+    const mainRef = this.mainRef.current;
+    const cardBackRef = this.cardBack.current;
+    container.classList.toggle("flipped");
+    container.classList.toggle("unflipped");
+    container.classList.toggle("full-screen");
+    mainRef.classList.toggle("filled");
+    cardBackRef.classList.toggle("table-toggle");
+    this.setState({ flipped: true });
   }
   render() {
-    const showProjects = this.state.flipped ? <Projects /> : "";
+    console.log(this.state.flipped);
+    const showProjects = this.state.flipped ? 
+    
+    <div>
+       <p className="featured-projects">FEATURED PROJECTS</p>
+      <Projects />
+      <div className="return-home" onClick={this.flipBackward}>
+              <i class="fas fa-chevron-circle-left contact-icon"></i>
+      </div>
+    </div> : "";
     return (
       <div
         className="flip-card card-container "
         ref={this.mainRef}
       >
         <div className="flip-card-inner unflipped shadow" ref={this.cardRef}>
-          <div className="flip-card-front shadow" onClick={this.handleClick}> 
+          <div className="flip-card-front shadow" onClick={this.flipForward}> 
             <div className="job-title">
               <span>Full Stack Software Engineer</span>
             </div>
@@ -77,12 +89,8 @@ class Card extends Component {
           </div>
           <div className="flip-card-back" ref={this.cardBack}>
             <Summary />
-
-            <p className="featured-projects">FEATURED PROJECTS</p>
             {showProjects}
-            <div className="return-home" onClick={this.handleClick}>
-              <i class="fas fa-chevron-circle-left contact-icon"></i>
-            </div>
+           
           </div>
         </div>
       </div>
